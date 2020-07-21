f7eade7196fe8c6edecbd3e6be38c41083fd58e6
harrisoncliew <41922141+harrisoncliew@users.noreply.github.com>
2020-07-21 11:27:02 -0700

    ExceptionsDemo, ExceptionMethods (IP)

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,12 @@
 package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
-	
+
+	public static double divide(double numerator, double denominator) {
+		if (denominator == 0) {
+			throw new IllegalArgumentException();
+		} else {
+			return numerator / denominator;
+		}
+	}
 }
